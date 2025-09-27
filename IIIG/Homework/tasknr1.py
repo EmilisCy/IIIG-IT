0d7ab792dcad43db77b675edfa5f23793ee542e7
EmilisCy <emilis.cybulskij.work@gmail.com>
2025-09-27 19:32:02 +0300

    first commit

---
+++ IIIG/Homework/tasknr1.py
@@ -0,0 +1,16 @@
+# 1. KET MOKYKLA
+def vairavimo_mokyklos_geriausi_pazymiai():
+    rezultatai = []
+    geriausi_rezultatai = []
+    with open('IIIG/duomenys1.txt', 'r') as f:
+        n = int(f.readline().strip())
+        for line in f:
+            rezultatai.append(list(map(int, line.split()))) 
+    for rezultatas in rezultatai:
+        geriausi_rezultatai.append(max(rezultatas))
+
+    with open('IIIG/duomenys2.txt', 'w') as e:
+        for reiksme in geriausi_rezultatai:
+            e.write(f'{reiksme}\n')
+
+vairavimo_mokyklos_geriausi_pazymiai()
