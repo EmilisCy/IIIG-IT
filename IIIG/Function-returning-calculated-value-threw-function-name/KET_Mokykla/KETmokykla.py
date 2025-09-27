89ba19cb908b523cb7e54f93855eeb079cc9bbb0
EmilisCy <emilis.cybulskij.work@gmail.com>
2025-09-28 01:11:21 +0300

    Pataisytas KET

--- IIIG/Function-returning-calculated-value-threw-function-name/KET_Mokykla/KETmokykla.py
+++ IIIG/Function-returning-calculated-value-threw-function-name/KET_Mokykla/KETmokykla.py
@@ -2,14 +2,14 @@
 def vairavimo_mokyklos_geriausi_pazymiai():
     rezultatai = []
     geriausi_rezultatai = []
-    with open('/Users/emiliscy/Desktop/Pamokos/IT/IIIG/Function-returning-calculated-value-threw-function-name/KET_Mokykla/duomenys1.txt', 'r') as f:
+    with open('IIIG/Function-returning-calculated-value-threw-function-name/KET_Mokykla/duomenys1.txt', 'r') as f:
         n = int(f.readline().strip())
         for line in f:
             rezultatai.append(list(map(int, line.split()))) 
     for rezultatas in rezultatai:
         geriausi_rezultatai.append(max(rezultatas))
 
-    with open('/Users/emiliscy/Desktop/Pamokos/IT/IIIG/Function-returning-calculated-value-threw-function-name/KET_Mokykla/duomenys2.txt', 'w') as e:
+    with open('IIIG/Function-returning-calculated-value-threw-function-name/KET_Mokykla/rezultatas1.txt', 'w') as e:
         for reiksme in geriausi_rezultatai:
             e.write(f'{reiksme}\n')
 
